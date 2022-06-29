dcafd43cedea7e2a8bc7cd308e3e61a966538b26
Anna <annarainchik@gmail.com>
2022-06-29 23:04:38 +0300

    new test

--- src/test/java/myandroid/FirstAndroidTest.java
+++ src/test/java/myandroid/FirstAndroidTest.java
@@ -16,48 +16,48 @@ public class FirstAndroidTest {
 
     @Test
     void searchTest() throws MalformedURLException, InterruptedException {
-       DesiredCapabilities caps = new DesiredCapabilities();
+        DesiredCapabilities caps = new DesiredCapabilities();
 
-     // Set your access credentials
+        // Set your access credentials
         caps.setCapability("browserstack.user", "autotestscloud_qAejV5");
-       caps.setCapability("browserstack.key", "DkvhzD6ZSLTsxYERedAR");
+        caps.setCapability("browserstack.key", "DkvhzD6ZSLTsxYERedAR");
 
-      // Set URL of the application under test
+        // Set URL of the application under test
         caps.setCapability("app", "bs://c700ce60cf13ae8ed97705a55b8e022f13c5827c");
 
-    // Specify device and os_version for testing
-       caps.setCapability("device", "Google Pixel 3");
-       caps.setCapability("os_version", "9.0");
+        // Specify device and os_version for testing
+        caps.setCapability("device", "Google Pixel 3");
+        caps.setCapability("os_version", "9.0");
 
-       // Set other BrowserStack capabilities
-       caps.setCapability("project", "First Java Project");
-       caps.setCapability("build", "browserstack-build-1");
-       caps.setCapability("name", "first_test");
+        // Set other BrowserStack capabilities
+        caps.setCapability("project", "First Java Project");
+        caps.setCapability("build", "browserstack-build-1");
+        caps.setCapability("name", "first_test");
 
-       // Initialise the remote Webdriver using BrowserStack remote URL
+        // Initialise the remote Webdriver using BrowserStack remote URL
         // and desired capabilities defined above
-       AndroidDriver<AndroidElement> driver = new AndroidDriver<AndroidElement>(
-               new URL("http://hub.browserstack.com/wd/hub"), caps);
-            //  new URL("http://hub-cloud.browserstack.com"), caps);
+        AndroidDriver<AndroidElement> driver = new AndroidDriver<AndroidElement>(
+                new URL("http://hub.browserstack.com/wd/hub"), caps);
+        //  new URL("http://hub-cloud.browserstack.com"), caps);
 
         // Test case for the BrowserStack sample Android app.
         // If you have uploaded your app, update the test case here.
-       AndroidElement searchElement = (AndroidElement) new WebDriverWait(driver, 30).until(
-              ExpectedConditions.elementToBeClickable(
-                       MobileBy.AccessibilityId("Search Wikipedia")));
+        AndroidElement searchElement = (AndroidElement) new WebDriverWait(driver, 30).until(
+                ExpectedConditions.elementToBeClickable(
+                        MobileBy.AccessibilityId("Search Wikipedia")));
         searchElement.click();
-       AndroidElement insertTextElement = (AndroidElement) new WebDriverWait(driver, 30).until(
-               ExpectedConditions.elementToBeClickable(
-                       MobileBy.id("org.wikipedia.alpha:id/search_src_text")));
-       insertTextElement.sendKeys("BrowserStack");
+        AndroidElement insertTextElement = (AndroidElement) new WebDriverWait(driver, 30).until(
+                ExpectedConditions.elementToBeClickable(
+                        MobileBy.id("org.wikipedia.alpha:id/search_src_text")));
+        insertTextElement.sendKeys("BrowserStack");
         Thread.sleep(5000);
         List<AndroidElement> allProductsName = driver.findElementsByClassName(
-               "android.widget.TextView");
-        assert(allProductsName.size() > 0);
+                "android.widget.TextView");
+        assert (allProductsName.size() > 0);
 
 
-      // Invoke driver.quit() after the test is done to indicate that the test is completed.
-      driver.quit();
+        // Invoke driver.quit() after the test is done to indicate that the test is completed.
+        driver.quit();
 
     }
 
